325ef5c65a50da2da1c439939e2fcb4cb296beed
kosuke-lab <k.12ap.m@gmail.com>
2021-09-24 14:46:54 +0900

    【1-7】記事モデルから記事情報を取得する

--- docker-laravel/backend/app/app/Http/Controllers/ArticleController.php
+++ docker-laravel/backend/app/app/Http/Controllers/ArticleController.php
@@ -2,25 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
     public function index()
     {
-        $articles = [
-            (object)
-            [
-                'id' => 1,
-                'title' => 'タイトル2',
-                'body' => '本文',
-                'created_at' => now(), 
-                'user' =>(object)[
-                    'id' => 2,
-                    'name' => 'ユーザー名1',
-             ]
-            ]
-        ];
+        // $articles = [
+        //     (object)
+        //     [
+        //         'id' => 1,
+        //         'title' => 'タイトル2',
+        //         'body' => '本文',
+        //         'created_at' => now(), 
+        //         'user' =>(object)[
+        //             'id' => 2,
+        //             'name' => 'ユーザー名1',
+        //      ]
+        //     ]
+        // ];
+
+         $articles = Article::all()->sortByDesc('created_at');
+
         return view ('articles.index',['article' => $articles]);   
      }
 }
